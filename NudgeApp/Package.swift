9ee7c00e73fcc79aa585967428fcd255f019745c
Aviad Danziger <aviadd@monday.com>
2026-06-18 00:50:50 +0300

    feat: scaffold NudgeApp SPM project

---
+++ NudgeApp/Package.swift
@@ -0,0 +1,12 @@
+// swift-tools-version: 6.0
+import PackageDescription
+
+let package = Package(
+    name: "Nudge",
+    targets: [
+        .executableTarget(
+            name: "Nudge",
+            path: "Sources/Nudge"
+        )
+    ]
+)
